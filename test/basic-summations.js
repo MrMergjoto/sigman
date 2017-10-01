a92b0762a0a945a851197b0539fff62557019aa9
Rich W <richw.sf@gmail.com>
2017-09-30 21:09:04 -0400

    added test, small changes

--- test/basic-summations.js
+++ test/basic-summations.js
@@ -6,23 +6,28 @@ var packagejson = require('./../package.json');
 
 describe('command line interface', function () {
 
-  it('1 to 5 Sigma i', function () {
+  it('1 to 5 sigma i', function () {
     let result = sigman(1, 5, "i");
     assert.equal(result, 15);
   });
 
-  it('1 to 3 Sigma i*i', function () {
+  it('1 to 3 sigma i*i', function () {
     let result = sigman(1, 3, "i*i");
     assert.equal(result, 14);
   });
 
-  it('1 to 3 Sigma 2*i*i', function () {
+  it('1 to 3 sigma 2*i*i', function () {
     let result = sigman(1, 3, "2*i*i");
     assert.equal(result, 28);
   });
-  
-  it('1 to 3 Sigma (2+i)*i', function () {
+
+  it('1 to 3 sigma (2+i)*i', function () {
     let result = sigman(1, 3, "(2+i)*i");
     assert.equal(result, 26);
+  });
+
+  it('1 to 4 sigma 1/(i*i) (note: funny rounding artifact)', function () {
+    let result = sigman(1, 4, "1/(i*i)");
+    assert.equal(result, 1.4236111111111112);
   });
 });
